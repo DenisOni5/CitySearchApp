007d611bd7a617886bd04abf2144bc48f873aa58
Den Oni <cgitester18@seznam.cz>
2022-11-20 17:38:23 +0100

    Added Unit Tests for CityCZ

---
+++ CitySearchApp.ApplicationUnitTests/CityCZ/Queries/GetCityCZListRequestHandlerTests.cs
@@ -0,0 +1,91 @@
+﻿using AutoMapper;
+using CitySearchApp.Application.Contracts.Persistance;
+using CitySearchApp.Application.DTOs;
+using CitySearchApp.Application.DTOs.SearchDTOs;
+using CitySearchApp.Application.Features.CityCZs.Handles.Queries;
+using CitySearchApp.Application.Features.CityCZs.Requests.Queries;
+using CitySearchApp.Application.Profiles;
+using CitySearchApp.ApplicationUnitTests.Mocks;
+using Moq;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CitySearchApp.ApplicationUnitTests.CityCZ.Queries
+{
+    public class GetCityCZListRequestHandlerTests
+    {
+        private readonly IMapper _mapper;
+        public GetCityCZListRequestHandlerTests()
+        {
+            var mapperConfig = new MapperConfiguration(c =>
+            {
+                c.AddProfile<MappingProfile>();
+            });
+
+            _mapper = mapperConfig.CreateMapper();
+        }
+
+        [Fact]
+        public async Task GetCityCZListTest()
+        {
+            CityLongSearchDto searchDto = new();
+
+            var _mockRepo = MockCityCZRepository.GetCityCZRepository(searchDto);
+
+            var hadler = new GetCityCZListRequestHandler(_mockRepo.Object, _mapper);
+
+            var result = await hadler.Handle(new GetCityCZListRequest { parameters = searchDto }, CancellationToken.None);
+
+            result.ShouldBeOfType<List<CityCZDto>>();
+
+            result.Count.ShouldBe(3);
+        }
+
+        [Fact]
+        public async Task GetCityCZCountTest()
+        {
+            CityShortSearchDto searchDto = new CityLongSearchDto();
+
+            var _mockRepo = MockCityCZRepository.GetCityCZRepository((CityLongSearchDto)searchDto);
+
+            var hadler = new GetCityCZCountRequestHandler(_mockRepo.Object);
+
+            var result = await hadler.Handle(new GetCityCZCountRequest {  shortSearchDto = searchDto  }, CancellationToken.None);
+
+            result.ShouldBe(3);
+        }
+
+        [Fact]
+        public async Task GetCityCZKrajeTest()
+        {
+            CityLongSearchDto searchDto = new();
+
+            var _mockRepo = MockCityCZRepository.GetCityCZRepository(searchDto);
+
+            var hadler = new GetCityCZKrajeRequestHandler(_mockRepo.Object);
+
+            var result = await hadler.Handle(new GetCityCZKrajeRequest(), CancellationToken.None);
+
+            result.Count.ShouldBe(2);
+
+            result.ShouldBeOfType<List<string>>();
+
+            result.ForEach(c => IsKrajTrue(c).ShouldBe(true));
+        }
+
+        public bool IsKrajTrue (string kraj)
+        {
+            return kraj switch
+            {
+                "Plzeňský kraj" => true,
+                "Hlavní město Praha" => true,
+                _ => false
+            };
+        }
+    }
+}
---
+++ CitySearchApp.ApplicationUnitTests/Mocks/MockCityCZRepository.cs
@@ -0,0 +1,69 @@
+﻿using CitySearchApp.Application.Contracts.Persistance;
+using CitySearchApp.Application.DTOs.SearchDTOs;
+using CitySearchApp.Domain;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CitySearchApp.ApplicationUnitTests.Mocks
+{
+    public static class MockCityCZRepository
+    {
+        public static Mock<ICityCZRepository> GetCityCZRepository(CityLongSearchDto searchDto)
+        {
+            var cities = new List<CitySearchApp.Domain.CityCZ>
+            {
+                new CitySearchApp.Domain.CityCZ
+                {
+                     Obec = "Bělá nad Radbuzou",
+                     ObecCode = "553441",
+                     Okres = "Domažlice",
+                     OkresCode = "CZ0321",
+                     Kraj = "Plzeňský kraj",
+                     KrajCode = "CZ032",
+                     PSC = "34526",
+                     Latitude = 49.591261,
+                     Longitude = 12.717718
+                },
+                new CitySearchApp.Domain.CityCZ
+                {
+                     Obec = "Chotěšov",
+                     ObecCode = "557838",
+                     Okres = "Plzeň-jih",
+                     OkresCode = "CZ0324",
+                     Kraj = "Plzeňský kraj",
+                     KrajCode = "CZ032",
+                     PSC = "33214",
+                     Latitude = 49.654190,
+                     Longitude = 13.202822
+                },
+                new CitySearchApp.Domain.CityCZ
+                {
+                     Obec = "Praha",
+                     ObecCode = "554782",
+                     Okres = "Praha",
+                     OkresCode = "CZ0100",
+                     Kraj = "Hlavní město Praha",
+                     KrajCode = "CZ010",
+                     PSC = "11000",
+                     Latitude = 50.075638,
+                     Longitude = 14.437900
+                }
+            };
+
+            var mockRepo = new Mock<ICityCZRepository>();
+
+            mockRepo.Setup(r => r.LoadCitiesWithParam(searchDto)).Returns(cities);
+
+            mockRepo.Setup(r => r.GetCityCount(searchDto)).Returns(cities.Count);
+
+            mockRepo.Setup(r => r.GetKraje()).Returns(cities.Select(c=>c.Kraj).Distinct().ToList());
+
+            return mockRepo;
+        }
+    }
+}
